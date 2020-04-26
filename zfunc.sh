91d7dd167775d14a507fe840f27eeda4176956f4
Zachary Conger <zachary.conger@stackhawk.com>
2020-04-26 12:41:31 -0600

    added a couple utility functions

--- zfunc.sh
+++ zfunc.sh
@@ -7,6 +7,17 @@ function zf_ec2_pub_ip {
   aws ec2 describe-instances --filters "Name=tag:Name,Values=${1}" --query "Reservations[*].Instances[*].PublicIpAddress" --output text
 }
 
+# SSH to a StackHawk EC2 instance by name as user ubuntu and with the shhawk1.pem key
+function zf_sshawk {
+  ssh -i ~/.ssh/sshawk1.pem ubuntu@$(zf_ec2_pub_ip "${1}")
+}
+
+function zf_dns_flush {
+  dscacheutil -flushcache
+  sudo killall -HUP mDNSResponder
+}
+
+
 ### Main script
 # Check to see if zfunc.sh was executed with an argument, and if so, try running it as a function
 if [[ -n $1 ]]; then
